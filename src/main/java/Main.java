ef7061c939fa139e51ec8a1f6c41838867e86556
idanylenko <idanylenko@talend.com>
2019-06-06 15:09:06 +0300

    added Task 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        Shape shape = new Shape("BROWN");
         Circle circle1 = new Circle("BLUE", 10);
-        System.out.println(shape);
-
-        System.out.println(shape.calcArea());
-        System.out.println(circle1.calcArea());
-        System.out.println(circle1);
 
         Rectangle rectangle1 = new Rectangle("YELLOW", 5, 2);
-        System.out.println(rectangle1);
-        System.out.println(rectangle1.calcArea());
 
         Triangle triangle1 = new Triangle("BLACK", 5, 5, 5);
-        System.out.println(triangle1);
-        System.out.println(triangle1.calcArea());
 
 
-        Rectangle rectangle2 = new Rectangle("YELLOW", 6, 2);
+        Rectangle rectangle2 = new Rectangle("ORANGE", 6, 2);
         Rectangle rectangle3 = new Rectangle("BLACK", 4, 2);
         Rectangle rectangle4 = new Rectangle("BROWN", 5, 3);
         Rectangle rectangle5 = new Rectangle("PURPLE", 5, 1);
@@ -27,6 +17,37 @@ public class Main {
         Triangle triangle2 = new Triangle("VIOLET", 7, 5, 4);
 
         Shape[] shapes = new Shape[]{rectangle1, rectangle2, rectangle3, rectangle4, rectangle5, circle1, circle2, triangle1, triangle2};
+
+        double sumArea = 0;
+        for (Shape elem1 : shapes) {
+            System.out.print(elem1.toString());
+            System.out.print(", area is: ");
+            System.out.println(elem1.calcArea());
+            sumArea += elem1.calcArea();
+        }
+
+        System.out.println("Total area is: " + sumArea);
+
+        double sumRectArea = 0;
+        double sumTriangleArea = 0;
+        double sumCircleArea = 0;
+
+        for (Shape elem2 : shapes) {
+            if (elem2 instanceof Rectangle) {
+                sumRectArea += elem2.calcArea();
+            }
+            if (elem2 instanceof Triangle) {
+                sumTriangleArea += elem2.calcArea();
+            }
+            if (elem2 instanceof Circle) {
+                sumCircleArea += elem2.calcArea();
+            }
+        }
+        System.out.println("Rectangles total area: " + sumRectArea);
+        System.out.println("Triangles total area: " + sumTriangleArea);
+        System.out.println("Circles total area: " + sumCircleArea);
+
+
 
 
 
